abf9d917af6624cae673d8f7fdf6f0b4c9686eaf
zorbasky <kyriakos.zorbas@arhs-dev-hellas.com>
2023-02-19 18:31:13 +0200

    Feature: Add description of the API with Swagger
    -The following class has been created:
      - SwaggerConfig
    
    -The following resource has been created:
      - swagger.yaml

---
+++ src/main/java/com/kyriakos/config/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.kyriakos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerConfig.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kyriakos.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider(
+            InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+        return () -> {
+            List<SwaggerResource> resources = new ArrayList<>();
+            Arrays.asList("api")
+                    .forEach(resourceName -> resources.add(loadResource(resourceName)));
+            return resources;
+        };
+    }
+
+    private SwaggerResource loadResource(String resource) {
+        SwaggerResource wsResource = new SwaggerResource();
+        wsResource.setName(resource);
+        wsResource.setSwaggerVersion("2.0");
+        wsResource.setLocation("/swagger/" + resource + "/swagger.yaml");
+        return wsResource;
+    }
+
+}
